b630d5f894b068814afc61afad8796cd07bab4e9
chensuzhen <1075929993@qq.com>
2016-06-29 13:53:30 +0800

    add MyRecyclerView

---
+++ MyRecyclerView/app/src/main/java/com/example/administrator/myrecyclerview/MyAdapter.java
@@ -0,0 +1,49 @@
+package com.example.administrator.myrecyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/6/28.
+ */
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+
+    private Context mContext;
+    private List<String> mDatas;
+
+    public MyAdapter(Context context, List<String> objects){
+        this.mContext = context;
+        this.mDatas = objects;
+    }
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        MyViewHolder holder = new MyViewHolder(LayoutInflater.from(
+                mContext).inflate(R.layout.item, parent,
+                false));
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.tv.setText(mDatas.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tv;
+        public MyViewHolder(View view) {
+            super(view);
+            tv = (TextView) view.findViewById(R.id.id_num);
+        }
+    }
+}
