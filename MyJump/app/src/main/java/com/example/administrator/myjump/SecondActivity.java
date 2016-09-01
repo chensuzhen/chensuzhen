133535b85decdc96feeefc15539c7dcb99c568fc
chensuzhen <1075929993@qq.com>
2016-09-01 15:44:29 +0800

    add MyJump

---
+++ MyJump/app/src/main/java/com/example/administrator/myjump/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.myjump;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/8/22.
+ */
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
